51f2d7ea8f20cc2b22877a15a3d5e7d760e1e158
Joao Antao <joao.antao@gmail.com>
2020-02-18 23:05:20 +0100

    Add solution for the compare triplets problem

---
+++ src/warmup/compareTriplets/Solution.kt
@@ -0,0 +1,23 @@
+package warmup.compareTriplets
+
+fun main() {
+    val result: Array<Int> = compareTriplets(arrayOf(17, 28, 30), arrayOf(99, 16, 8))
+    println(result.joinToString(" "))
+}
+
+private fun compareTriplets(a: Array<Int>, b: Array<Int>): Array<Int> {
+    var alicePoints = 0
+    var bobPoints = 0
+
+    for (x in 0..2) {
+        val itemAlice = a[x]
+        val itemBob = b[x]
+        if (itemAlice > itemBob)
+            alicePoints++
+
+        if (itemBob > itemAlice)
+            bobPoints++
+    }
+
+    return arrayOf(alicePoints, bobPoints)
+}
