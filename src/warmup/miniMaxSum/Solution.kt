2861627b496f8663d0aee81d7d039ab49b8e3758
Joao Antao <joaoantao@funda.nl>
2020-02-18 18:37:31 +0100

    Add solution for mini-max problem

---
+++ src/warmup/miniMaxSum/Solution.kt
@@ -0,0 +1,10 @@
+package warmup.miniMaxSum
+
+fun main() {
+    miniMaxSum(arrayOf(256741038, 623958417, 467905213, 714532089, 938071625))
+}
+
+private fun miniMaxSum(arr: Array<Long>): Unit {
+    arr.sort() // I feel like cheating here, but sort() is available
+    println("${arr.take(arr.size - 1).sum()} ${arr.drop(1).sum()}")
+}
