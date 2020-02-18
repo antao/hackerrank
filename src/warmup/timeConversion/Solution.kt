8d4b257a3e97210775e33f32e6d0d9c8014db345
Joao Antao <joao.antao@gmail.com>
2020-02-18 22:24:22 +0100

    Add solution for time conversion problem

---
+++ src/warmup/timeConversion/Solution.kt
@@ -0,0 +1,17 @@
+package warmup.timeConversion
+
+fun main() {
+    println(timeConversion("07:05:45PM"))
+}
+
+private fun timeConversion(s: String): String {
+    val clockSystem = s.substring(8)
+    val hours = s.substring(0,2)
+    val remainingTime = s.substring(2, 8)
+
+    return when {
+        clockSystem == "AM" && hours == "12" -> "00$remainingTime"
+        clockSystem == "PM" && hours != "12" -> "${12 + hours.toInt()}$remainingTime"
+        else -> (hours + remainingTime)
+    }
+}
