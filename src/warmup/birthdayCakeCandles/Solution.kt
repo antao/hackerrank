754d4478b49b5e36e5a08194489070235e7660dd
Joao Antao <joao.antao@gmail.com>
2020-02-19 20:30:59 +0100

    Add solution for birthday cake candles problem

---
+++ src/warmup/birthdayCakeCandles/Solution.kt
@@ -0,0 +1,23 @@
+package warmup.birthdayCakeCandles
+
+fun main() {
+    println(birthdayCakeCandles(arrayOf(3, 2, 1, 3)))
+}
+
+private fun birthdayCakeCandles(ar: Array<Int>): Int {
+    var max: Int = ar[0];
+    var result: Int = 1;
+
+    for (x in 1 until ar.size) {
+        if (ar[x] == max) {
+            result++
+            continue
+        }
+        if (ar[x] > max) {
+            max = ar[x]
+            result = 1
+        }
+    }
+
+    return result
+}
