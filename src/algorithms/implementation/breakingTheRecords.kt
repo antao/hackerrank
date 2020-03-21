9c7750672bffbdd5d99f9b12b4515c6ae5daf63e
Joao Antao <joao.antao@gmail.com>
2020-03-21 14:12:10 +0100

    Add solution for breaking the records problem

---
+++ src/algorithms/implementation/breakingTheRecords.kt
@@ -0,0 +1,35 @@
+import java.util.*
+
+// Complete the breakingRecords function below.
+fun breakingRecords(scores: Array<Int>): Array<Int> {
+    var mostPoints = scores[0]
+    var leastPoints = scores[0]
+    var mostPointsBreak = 0
+    var leastPointsBreak = 0
+
+    for (i in 1 until scores.size) {
+        val points = scores[i]
+        if (points > mostPoints) {
+            mostPoints = points
+            mostPointsBreak += 1
+        }
+        if (points < leastPoints) {
+            leastPoints = points
+            leastPointsBreak += 1
+        }
+    }
+
+    return arrayOf(mostPointsBreak, leastPointsBreak)
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val scores = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = breakingRecords(scores)
+
+    println(result.joinToString(" "))
+}
