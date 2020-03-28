4b53c904e0447d356c549b7468d245b1179a7af3
Joao Antao <joao.antao@gmail.com>
2020-03-28 19:28:43 +0100

    Add recursion lecture for 30 days of code

---
+++ src/thirthyDaysOfCode/recursionDay9.kt
@@ -0,0 +1,21 @@
+package thirthyDaysOfCode
+
+import java.util.*
+
+// Complete the factorial function below.
+fun factorial(n: Int): Int {
+    return if (n >= 1)
+        n * factorial(n - 1)
+    else
+        1
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val result = factorial(n)
+
+    println(result)
+}
